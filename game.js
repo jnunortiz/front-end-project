b22ce70434eda1d23cbe2ee0548cff419b49f7e2
jnunortiz <jnunortiz@gmail.com>
2017-11-08 21:12:56 +0100

    Fixing restart button to not add count on moves

--- game.js
+++ game.js
@@ -112,6 +112,7 @@ function game() {
     $('.metrics').text("Number of moves: " + moves)
     var clickDisabled = false;
     $("a").click(function() {
+        if ($(this).text() === "Restart") { return; };
         /* Generates the events after clicking */
         if (clickDisabled)
             return;
